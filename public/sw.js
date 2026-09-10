cdc1e77632d66628cb31867efd04201ac3cbe4e8
Muneeb Anjum <125146931+muneeb-anjum0@users.noreply.github.com>
2026-09-10 17:51:49 +0500

    Release HisabKitab v1.0.6

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v10';
+const VERSION = 'v11';
 const SHELL_CACHE = `hisabkitab-shell-${VERSION}`;
 const ASSET_CACHE = `hisabkitab-assets-${VERSION}`;
 const FONT_CACHE = 'hisabkitab-fonts-v1';
